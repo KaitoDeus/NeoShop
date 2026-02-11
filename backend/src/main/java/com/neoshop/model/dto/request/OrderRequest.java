f0cda7037fe938c4517368bcdc8d0eefefcd346e
Khải Võ <kenkaneki395@gmail.com>
2026-02-11 16:44:03 +0700

    feat(backend): add DTO request and response for order and category

---
+++ backend/src/main/java/com/neoshop/model/dto/request/OrderRequest.java
@@ -0,0 +1,26 @@
+package com.neoshop.model.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderRequest {
+    private String paymentMethod;
+    private String shippingAddress;
+    private List<OrderItemRequest> items;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class OrderItemRequest {
+        private UUID productId;
+        private Integer quantity;
+    }
+}
